3f978a8faf6254c324ea7a26f437ed92addc2aee
Harshit Gulati <harshit14012018@gmail.com>
2025-05-22 11:49:51 +0000

    Console Error
    In HTML, whitespace text nodes cannot be a child of <html>. Make sure you don't have any extra whitespace between tags on each line of your source code.
    This will cause a hydration error.
    
    See more info here: https://nextjs.org/docs/messages/react-hydration-error
    
    ...
        <HotReload assetPrefix="" globalError={[...]}>
          <AppDevOverlay state={{nextId:1, ...}} globalError={[...]}>
            <AppDevOverlayErrorBoundary globalError={[...]} onError={function bound dispatchSetState}>
              <ReplaySsrOnlyErrors>
              <DevRootHTTPAccessFallbackBoundary>
                <HTTPAccessFallbackBoundary notFound={<NotAllowedRootHTTPFallbackError>}>
                  <HTTPAccessFallbackErrorBoundary pathname="/" notFound={<NotAllowedRootHTTPFallbackError>} ...>
                    <RedirectBoundary>
                      <RedirectErrorBoundary router={{...}}>
                        <Head>
                        <link>
                        <script>
                        <script>
                        <script>
                        <script>
                        <script>
                        <RootLayout>
    >                     <html lang="en" suppressHydrationWarning={true}>
    >                       {" "}
                            ...
                        ...
            ...
    src/app/layout.tsx (30:5) @ RootLayout
    
      28 | }>) {
      29 |   return (
    > 30 |     <html lang="en" suppressHydrationWarning> {/* Added suppressHydrationWarning for next-themes */}
         |     ^
      31 |       <body className={`${geistSans.variable} ${geistMono.variable} antialiased font-sans`}>
      32 |         <ThemeProvider
      33 |           attribute="class"
    Call Stack
    14
    
    Show 12 ignore-listed frame(s)
    html
    <anonymous> (0:0)
    RootLayout
    src/app/layout.tsx (30:5)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning> {/* Added suppressHydrationWarning for next-themes */}
+    <html lang="en" suppressHydrationWarning>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased font-sans`}>
         <ThemeProvider
           attribute="class"
